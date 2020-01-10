2425dd2ace6fa0a9c9dc4ff19dace95e293a7e4b
Cory Chambers <corytyme@yahoo.com>
2020-01-10 06:55:13 -0500

    created store and provided it to the application

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
 
+//this will be our main root reducer
 export default combineReducers({
   authedUser,
   users,
